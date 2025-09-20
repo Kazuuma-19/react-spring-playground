e93f9ad53df65d021c466e79a703463e58360414
Kazuma <kazu15870@gmail.com>
2025-09-20 10:46:01 +0900

    feat: qodanaの差分検出テスト

---
+++ backend/src/main/java/com/example/backend/QodanaTest.java
@@ -0,0 +1,8 @@
+package com.example.backend;
+
+public class QodanaTest {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
